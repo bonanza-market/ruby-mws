e175be6bea53a83eca95526df0863da6f5060b68
Marek Cifra <cifra@cifra.sk>
2024-05-06 12:06:24 +0200

    update to monkey patch camelize method
    now support original camelize method behaviour

--- lib/ruby-mws.rb
+++ lib/ruby-mws.rb
@@ -37,8 +37,13 @@ end
 
 class String
 
+  alias :old_camelize :camelize
+
   def camelize(first_letter_in_uppercase = true)
-    if first_letter_in_uppercase
+    # this monkey patch is ommiting original camelize method behavior, possible symbol values are :lower and :upper (which is default)
+    if first_letter_in_uppercase.kind_of?(Symbol)
+      old_camelize(first_letter_in_uppercase)
+    elsif first_letter_in_uppercase
       self.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
     else
       self.to_s[0].chr.downcase + camelize(lower_case_and_underscored_word)[1..-1]
